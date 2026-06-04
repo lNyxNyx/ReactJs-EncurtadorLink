dfd9a50b036c1e46afcd45d955870626e55d83e4
unknown <junior-sid99@hotmail.com>
2026-06-04 20:17:03 -0300

    rm debug

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 const key = process.env.REACT_APP_TOKEN;
 
-console.log(key);
-
 const api = axios.create({
     baseURL: 'https://api-ssl.bitly.com/v4',
     headers: {
